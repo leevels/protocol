4c60c716d0066ddd2509d18f914c7498db75db91
Xiangmin Liu <635750556@qq.com>
2018-12-16 21:01:36 +0800

    container support coroutine

--- Coroutine.php
+++ Coroutine.php
@@ -20,8 +20,11 @@ declare(strict_types=1);
 
 namespace Leevel\Protocol;
 
+use Leevel\Di\ICoroutine;
+use Swoole\Coroutine as SwooleCoroutine;
+
 /**
- * 协议.
+ * 协程实现.
  *
  * @author Xiangmin Liu <635750556@qq.com>
  *
@@ -29,6 +32,38 @@ namespace Leevel\Protocol;
  *
  * @version 1.0
  */
-class Coroutine
+class Coroutine implements ICoroutine
 {
+    /**
+     * 是否处于协程上下文.
+     *
+     * @param string $className
+     */
+    public function context(string $className): bool
+    {
+        if (!class_exists($className)) {
+            return false;
+        }
+
+        if (method_exists($className, 'coroutineContext') &&
+            true === $className::coroutineContext()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 当前协程 ID.
+     *
+     * @return int
+     */
+    public function uid(): int
+    {
+        if (class_exits('\\Swoole\\Coroutine')) {
+            return -1;
+        }
+
+        return SwooleCoroutine::getuid();
+    }
 }
